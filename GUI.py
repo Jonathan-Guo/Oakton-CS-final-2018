b011709dd61666cdc53f8925aa904f0e880278db
iugbhjvhgvh <justin.guo01@gmail.com>
2018-05-18 11:24:42 -0400

    fixed bug where lines jump after drawing again; changed variable names

--- GUI.py
+++ GUI.py
@@ -3,31 +3,73 @@ from tkinter import *
 root = Tk()
 xPos = 0.0
 yPos = 0.0
+createStarting = 1.0
+line_list = [[]]
+
+
+class Vertex:
+    x = 0
+    y = 0
+
+    def __init__(self, event_x, event_y):
+        self.x = event_x
+        self.y = event_y
+
+    def get_x(self):
+        return self.x
+
+    def get_y(self):
+        return self.y
+
+    def set_x(self, event_x):
+        self.x = event_x
+
+    def set_y(self, event_y):
+        self.x = event_y
 
 
 def draw(event):
     global xPos
     global yPos
+    global line_list
     if xPos != 0.0 and yPos != 0.0:
         canvas.create_line(xPos, yPos, event.x, event.y, fill="black")
         xPos = event.x
         yPos = event.y
+        line_list[-1].append(Vertex(xPos, yPos))
     else:
         xPos = event.x
         yPos = event.y
+        line_list[-1].append(Vertex(xPos, yPos))
 
 
-def reset(event):
+def reset_draw(event):
     global xPos
     global yPos
     xPos = 0.0
     yPos = 0.0
 
 
+def create_start(event):
+    global createStarting
+    if createStarting == 1.0:
+        canvas.create_oval(event.x, event.y, event.x+10, event.y+10, fill="red")
+        createStarting = 2.0
+    elif createStarting == 2.0:
+        canvas.create_oval(event.x, event.y, event.x+10, event.y+10, fill="green")
+        createStarting = 0.0
+
+
+def create_line_representation(event):
+    global line_list
+    line_list[-1].append(Vertex(event.x, event.y))
+
+
 canvas = Canvas(root, width=500, height=500)
 canvas.bind("<B1-Motion>", draw)
-canvas.bind("<ButtonRelease-1>", reset)
-canvas.create_rectangle(0, 0, 500, 500, fill='red')
+canvas.bind("<ButtonRelease-1>", reset_draw)
+canvas.bind("<Button-3>", create_start)
+canvas.create_rectangle(0, 0, 500, 500, fill='white')
 
 canvas.pack()
 root.mainloop()
